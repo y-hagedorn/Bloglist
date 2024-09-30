10126519557b0192eea078569c6986691b1fb3fa
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-30 15:45:41 +0200

    Exercise 4.4 Bloglist: totalLikes Unit Tests

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -3,8 +3,8 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-    const blogs = []
+  const blogs = []
 
-    const result = listHelper.dummy(blogs)
-    assert.strictEqual(result, 1)
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
 })
