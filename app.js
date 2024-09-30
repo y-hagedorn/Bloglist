f1a962ab0f015e584dafd350ce3ec20b2d7bd1a6
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-30 14:02:08 +0200

    Exercise 4.2 Bloglist: Refactor the application into separate modules

---
+++ app.js
@@ -0,0 +1,34 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+//mongoose.set('strictQuery', false)
+
+//logger.info('connecting to', config.MONGODB_URI)
+
+//const Blog = mongoose.model('Blog', blogSchema)
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+//app.use(express.static('dist'))
+app.use(express.json())
+
+// Router
+app.use('/api/blogs', blogsRouter)
+
+// Middleware
+// ...
+
+module.exports = app
