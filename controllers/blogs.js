787bb044de1609ab01e8a5e704db44b3e15c106d
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-10-11 22:35:02 +0200

    Exercise 5.9: Correction for the problem that when a blog is liked in the app, the name of the user that added the blog is not shown in its details anymore - ensure that the user field is fully populated before returning the blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -57,8 +57,9 @@ blogsRouter.put('/:id', async (request, response) => {
   }
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .populate('user', { username: 1, name: 1 })
   if (!updatedBlog) {
-    return response.status(404).json( {error: 'Blog not found'} )
+    return response.status(404).json({ error: 'Blog not found' })
   }
   response.json(updatedBlog)
 })
